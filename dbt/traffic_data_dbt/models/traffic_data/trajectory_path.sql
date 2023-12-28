f949b2a85dd1522e46e66f20f6b1bdb1db90f44f
MubarekHussen <mubahussen2014@gmail.com>
2023-12-28 23:57:38 +0300

    refac: adding a sequence

--- dbt/traffic_data_dbt/models/traffic_data/trajectory_path.sql
+++ dbt/traffic_data_dbt/models/traffic_data/trajectory_path.sql
@@ -1,18 +1,25 @@
 {{ config(
     materialized='table',
-    alias='trajectory_path'
+    alias='trajectory_path_for_viz'
 ) }}
 
-WITH trajectory_path AS (
+WITH ordered_trajectory AS (
     SELECT
         vp.track_id,
         vp.time,
         vp.lat,
         vp.lon,
-        ti.type
+        ti.type,
+        ROW_NUMBER() OVER (PARTITION BY vp.track_id ORDER BY vp.time) AS point_sequence
     FROM {{ source('traffic_data', 'vehicle_positions') }} vp
     JOIN {{ source('traffic_data', 'trajectory_info') }} ti ON vp.track_id = ti.track_id
 )
 
-SELECT *
-FROM trajectory_path
+SELECT
+    track_id,
+    time,
+    lat,
+    lon,
+    type,
+    point_sequence
+FROM ordered_trajectory
